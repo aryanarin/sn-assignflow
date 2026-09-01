9ffe6ae59101d707fb3ed3aa33d740f6a76ec125
Aryan Raj <96729614+aryanarin@users.noreply.github.com>
2026-09-01 16:25:29 +0200

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('btnConfig').addEventListener('click', function () {
-    browser.tabs.create({ url: browser.runtime.getURL('config.html') });
-    window.close();
-  });
-});
